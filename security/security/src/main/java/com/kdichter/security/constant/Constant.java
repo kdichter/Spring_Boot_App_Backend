28939f16af243204aa62b73f384db10858b867fb
Kevin Dichter <kevinidichter@gmail.com>
2026-02-25 15:35:09 -0500

    added docker files
    added a docker file and a docker compose file

--- security/security/src/main/java/com/kdichter/security/constant/Constant.java
+++ security/security/src/main/java/com/kdichter/security/constant/Constant.java
@@ -2,6 +2,6 @@ package com.kdichter.security.constant;
 
 public class Constant {
     public static final String PHOTO_DIRECTORY = System.getProperty("user.home") + "/Downloads/uploads/";
-    public static final String WEB_URL = "http://localhost:3000";
+    public static final String WEB_URL = System.getenv().getOrDefault("WEB_URL", "http://localhost:3000");
     public static final String X_REQUESTED_WITH = "X-Requested-With";
 }
