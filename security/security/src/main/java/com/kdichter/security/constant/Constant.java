9229a802059cea15f7932aa9153da81032d7c23f
Kevin Dichter <kevinidichter@gmail.com>
2026-02-26 12:04:26 -0500

    added github action
    added a github action and updated the docker-compose file

--- security/security/src/main/java/com/kdichter/security/constant/Constant.java
+++ security/security/src/main/java/com/kdichter/security/constant/Constant.java
@@ -1,7 +1,7 @@
 package com.kdichter.security.constant;
 
 public class Constant {
-    public static final String PHOTO_DIRECTORY = System.getProperty("user.home") + "/Downloads/uploads/";
+    public static final String PHOTO_DIRECTORY = System.getenv().getOrDefault("UPLOAD_DIR", System.getProperty("user.home") + "/Downloads/uploads/");
     public static final String WEB_URL = System.getenv().getOrDefault("WEB_URL", "http://localhost:3000");
     public static final String X_REQUESTED_WITH = "X-Requested-With";
 }
